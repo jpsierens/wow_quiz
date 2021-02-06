8d8609f1d6a9099dfeee857fe160a2064d263f54
jpsierens <jpsierens1988@gmail.com>
2021-02-06 17:33:34 -0600

    improve tests

--- spec/wow_quiz_spec.rb
+++ spec/wow_quiz_spec.rb
@@ -19,6 +19,17 @@ describe "wow quiz" do
         end
     end
 
+    describe "enters correct classes but with different casing" do
+        it "passes the test since we dont care about casing" do
+            classes = CLASSES.clone
+            # push to the array so we dont have to write all the classes here, it will get downcased and uniq'd later
+            # so if we do have the same word, it will end up with the correct array of classes (e.g. pRiEst => priest)
+            input = classes.push("pRiEst").join(" ")
+            allow($stdin).to receive(:gets).and_return(input)
+            expect { play }.to output(/Correct!! Thanks for playing/).to_stdout
+        end
+    end
+
     describe "enters everything wrong" do
         it "tells you what's wrong" do
             input = "Bard SpellBreaker Necromancer"
@@ -32,21 +43,19 @@ describe "wow quiz" do
     describe "enters partially correct" do
         it "tells you what is correct and what is wrong" do
             input = "Priest Paladin Bard"
-            expected_correct = /You have the following classes correct: priest paladin. Retype them and the missing ones/
-            expected_wrong = /The following classes are wrong: bard/
+            expected = /The following classes are wrong: bard\nThe following classes are right: priest paladin/
             allow($stdin).to receive(:gets).and_return(input, 'quit')
-            expect { play }.to output(Regexp.union(expected_correct, expected_wrong)).to_stdout
+            expect { play }.to output(expected).to_stdout
         end
-    end
 
-    describe "enters correct classes but with different casing" do
-        it "passes the test since we dont care about casing" do
-            classes = CLASSES.clone
-            # push to the array so we dont have to write all the classes here, it will get downcased and uniq'd later
-            # so if we do have the same word, it will end up with the correct array of classes (e.g. pRiEst => priest)
-            input = classes.push("pRiEst").join(" ")
-            allow($stdin).to receive(:gets).and_return(input)
-            expect { play }.to output(/Correct!! Thanks for playing/).to_stdout
+        describe "after entering partially correct and retrying" do
+            it "tells you what is correct so far and the number of missing classes" do 
+                input = "Priest Paladin bard"
+                input2 = "Monk"
+                expected = /You have the following classes correct: monk priest paladin. You are missing 8 classes/
+                allow($stdin).to receive(:gets).and_return(input, input2, 'quit')
+                expect { play }.to output(expected).to_stdout
+            end
         end
     end
 end
